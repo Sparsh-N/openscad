bb9d0ff63cbfc5eaf55adc3c16c791d66468ef4d
Sparsh-N <nairspar@iu.edu>
2025-07-24 03:13:16 -0400

    more updates will rebase soon

--- scripts/build_vcpkg.bat
+++ scripts/build_vcpkg.bat
@@ -18,7 +18,7 @@ rem Find vcpkg root first so that we can bail early if it's not found.
 @REM     echo Set VCPKG_ROOT to %VCPKG_ROOT%
 @REM )
 
-rem vcpkg install --triplet x64-windows # already ran once, it works
+vcpkg install --triplet x64-windows rem already ran once, it works
 mkdir build
 cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=C:/vcpkg/scripts/buildsystems/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x64-windows -DUSE_BUILTIN_OPENCSG=TRUE -DENABLE_CAIRO=FALSE -DHEADLESS=ON -DCMAKE_EXE_LINKER_FLAGS="/manifest:no" -DCMAKE_MODULE_LINKER_FLAGS="/manifest:no" -DCMAKE_SHARED_LINKER_FLAGS="/manifest:no"
 cmake --build build
