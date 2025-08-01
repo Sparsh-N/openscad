717ca6b4b0ad1e3f7aa35badb73ec42273aa4235
Sparsh-N <nairspar@iu.edu>
2025-08-01 04:48:24 -0400

    update headless branch, gui based build will be separate branch

--- scripts/build_vcpkg.bat
+++ scripts/build_vcpkg.bat
@@ -21,7 +21,7 @@ rem Find vcpkg root first so that we can bail early if it's not found.
 vcpkg install --triplet x64-windows rem already ran once, it works
 mkdir build
 cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=C:/vcpkg/scripts/buildsystems/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x64-windows -DUSE_BUILTIN_OPENCSG=TRUE -DENABLE_CAIRO=FALSE -DHEADLESS=ON -DCMAKE_EXE_LINKER_FLAGS="/manifest:no" -DCMAKE_MODULE_LINKER_FLAGS="/manifest:no" -DCMAKE_SHARED_LINKER_FLAGS="/manifest:no"
-cmake --build build
+rem cmake --build build --config Debug
 @REM cmake -B build -S . ^
 @REM     -DCMAKE_TOOLCHAIN_FILE=C:/vcpkg/scripts/buildsystems/vcpkg.cmake ^
 @REM     -DVCPKG_TARGET_TRIPLET=x64-windows ^
